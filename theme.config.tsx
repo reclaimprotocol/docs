1d302083bdc7fe8f860643f3887b6597e77ee711
Sweta Shaw <swt.shw94@gmail.com>
2023-08-18 13:15:06 +0530

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,16 +3,26 @@ import Image from 'next/image'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-    logo: <span className="font-extrabold">
-        {/* <Image src="/assets/logo.png" alt="logo"  width={20} height={20} /> */}
-        Reclaim
+    logo: <span className="font-extrabold flex items-center gap-2">
+        <Image src="/assets/logo.png" alt="logo"  width={50} height={50} />
+        <p className="text-xl">Reclaim</p>
         </span>,
+    head: (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Reclaim Developer Docs" />
+        <meta name="keywords" content="reclaim, reclaim protocol, zero-knowledge, zk, identity, decentralised, deco, tls"></meta>
+        <meta property="og:description" content="Empower Apps, Empower Users: Reclaim SDK Toolkit Unleashes Data Sovereignty" />
+      </>
+    ),
+    darkMode: true,
     project: {
       link: 'https://github.com/reclaimprotocol',
     },
-    // chat: {
-    //   link: 'https://gmail.com',
-    // },
+    chat: {
+      link: 'https://t.me/abhilashinumella',
+      icon: <Image src='/assets/icons/tg.png' width={30} height={30} alt='contact'/>
+    },
     docsRepositoryBase: 'https://github.com/reclaimprotocol',
     footer: {
       text: 'Reclaim Protocol © 2021',
