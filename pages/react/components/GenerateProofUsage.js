a18005432e59a1cf3f317d087fea26b8f04c0c3c
Dip Bhattacharjee <dip.bhattacharjee@postman.com@Dips-MacBook-Pro-2.local>
2023-10-26 23:25:30 +0530

    Added generateProof component documentation

---
+++ pages/react/components/GenerateProofUsage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { GenerateProof } from '@reclaimprotocol/react-components';
+
+function GenerateProofUsage () {
+  return (
+    <GenerateProof
+      appID='6d6c04eb-237b-4599-8797-94d48b0ac612'
+      userID='dasq2easdase-asdq2e3'
+      onProofSubmission={() => {}}
+      onProofSubmissionFailed={() => {}}
+    />
+  );
+}
+
+export default GenerateProofUsage;
