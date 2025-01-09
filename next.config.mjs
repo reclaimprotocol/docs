4bd255db4df011b143f17d8b61762c61449029bf
Adithya Krishna <aadithya794@gmail.com>
2025-01-09 14:25:25 +0530

    chore: update redirect

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,11 @@ const config = {
       source: '/advance-configurations/advance-options',
       destination: '/advance-options',
       permanent: true
+    },
+    {
+      source: '/sdk/polkadot/substrate-quickstart',
+      destination: '/sdk/polkadot/quickstart',
+      permanent: true
     }
   ]
 };
