da4f82ce6459c85c830d77b4ef0e63def28b189d
Adithya Krishna <aadithya794@gmail.com>
2025-01-09 14:43:53 +0530

    chore: update redirects

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,56 @@ const config = {
       source: '/sdk/polkadot/substrate-quickstart',
       destination: '/sdk/polkadot/quickstart',
       permanent: true
+    },
+    {
+      source: '/sdk-methods',
+      destination: '/',
+      permanent: true
+    },
+    {
+      source: '/js/backend',
+      destination: '/backend',
+      permanent: true
+    },
+    {
+      source: '/solidity/supported-networks',
+      destination: '/sdk/solidity/supported-networks',
+      permanent: true
+    },
+    {
+      source: '/install',
+      destination: '/#getting-started',
+      permanent: true
+    },
+    {
+      source: '/js/frontend',
+      destination: '/js/installation',
+      permanent: true
+    },
+    {
+      source: '/zkfetch',
+      destination: '/sdk/zkfetch',
+      permanent: true
+    },
+    {
+      source: '/security',
+      destination: '/',
+      permanent: true
+    },
+    {
+      source: '/api',
+      destination: '/',
+      permanent: true
+    },
+    {
+      source: '/whitepaper-and-how-reclaim-protocol-works',
+      destination: '/#overview',
+      permanent: true
+    },
+    {
+      source: '/node/quickstart',
+      destination: '/backend',
+      permanent: true
     }
   ]
 };
