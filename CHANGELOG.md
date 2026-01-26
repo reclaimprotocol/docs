281bec23b8cd4caa7915ab73475dcf6f9fb2a09e
Mushaheed Syed <mushaheed.syed@proton.me>
2026-01-26 16:41:38 -0500

    Added log event type documentation for inapp sdk and inapp react native sdk (#100)

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.0
+
+* Added log event type documentation for inapp sdk and inapp react native sdk
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,13 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+export const metadata = {
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
